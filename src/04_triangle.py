966a304b2a07c4a16fa093e9fac724031fde3f78
Weit1sh <Igortaras240209@gmail.com>
2023-02-06 20:10:33 +0200

    feat: all geomerty tasks done

---
+++ src/04_triangle.py
@@ -0,0 +1,6 @@
+print("Enter the triange ")
+tr = int(input(">>> "))
+print("*")
+for i in range(tr - 2):
+    print("*" + " " * (i) + "*")
+print("*" * tr)
---
+++ src/05_rectangle.py
@@ -0,0 +1,14 @@
+print("Enter the smaller side of the rectangle ")
+sm = int(input(">>> "))
+print("Enter the bigger side of the rectangle ")
+bg = int(input(">>> "))
+for i in range(1, sm + 1):
+    print("*" * bg)
+
+print ("------------")
+
+
+print("*" * bg)
+for i in range(1, sm - 1):
+    print("*" + " " * (bg - 2) + "*")
+print("*" * bg)
